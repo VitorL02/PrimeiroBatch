9fef2f8086da4d37d561014ac847b91c3efb2227
Vitor Lucas <vitorcrispim@yahoo.com.br>
2024-06-17 23:27:06 -0300

    criando o primeiro batch chunk

--- src/main/java/com/spring/batch/primeirobatch/BatchConfig.java
+++ src/main/java/com/spring/batch/primeirobatch/BatchConfig.java
@@ -3,6 +3,7 @@ package com.spring.batch.primeirobatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -10,26 +11,41 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 
 @Configuration
 public class BatchConfig {
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private PlatformTransactionManager transactionManager ;
 
     @Bean
-    public Job imprimeOlaJob(JobRepository jobRepository, Step step) {
+    public Job imprimeOlaJob(Step step) {
         return new JobBuilder("primeiroJob", jobRepository)
                 .start(step)
+                .next(imprimeParImparStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager, Tasklet imprimeStringTasklet) {
+    public Step step(Tasklet imprimeStringTasklet) {
         return new StepBuilder("step", jobRepository)
                 .tasklet(imprimeStringTasklet, transactionManager)
                 .build();
@@ -54,5 +70,27 @@ public class BatchConfig {
             System.out.println("Ola " + name);
             return RepeatStatus.FINISHED;
         }
+    }
+    @Bean
+    public Step imprimeParImparStep(){
+        return new StepBuilder("imprimeParImpar",jobRepository)
+                .<Integer,String>chunk(1,transactionManager)
+                .reader(contaAteDezReader())
+                .processor(processaContador())
+                .writer(imprimeWriter())
+                .build();
+    }
+
+    private ItemWriter<String> imprimeWriter() {
+        return itens -> itens.forEach(System.out::println);
+    }
+
+    private FunctionItemProcessor<Integer, String> processaContador() {
+        return new FunctionItemProcessor<Integer,String>(item -> item % 2 == 0 ? String.format("Item %s é Par",item) : String.format("Item %s é Impar",item));
+    }
+
+    private IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numerosUmAteDez = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        return new IteratorItemReader<Integer>(numerosUmAteDez.iterator());
     }
 }
